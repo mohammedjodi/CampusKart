25197399326cdc89c7e35219999c67df6b19bd8c
mohammedjodi <mohammedjodi457@gmail.com>
2026-09-01 11:17:55 +0100

    added database tables and migrations... and Updated ReadMe...

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,6 @@ $routes->get('/', 'Home::index');
 service('auth')->routes($routes , ['except' => ['register']]);
 //custom routes for shield 
 $routes->get('register' , '\App\Controllers\Auth\RegisterController::registerView');
+
+service('auth')->routes($routes);
 $routes->post('register' , '\App\Controllers\Auth\RegisterController::registerAction');
